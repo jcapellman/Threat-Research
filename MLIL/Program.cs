2afad0b1b7b4c35b829c23f523c5dd26a71d51f4
Jarred Capellman <jcapellman@hotmail.com>
2020-12-17 20:16:38 -0600

    Added: Initial MLIL work

---
+++ MLIL/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ICSharpCode.Decompiler;
+
+namespace MLIL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void Decompile(string fileName)
+        {
+            
+        }
+    }
+}
