403e4c90ffadf0d6c02cedfa731276107516bae1
Jarred Capellman <jcapellman@hotmail.com>
2021-01-22 18:13:58 -0600

    Added: Initial work

---
+++ EEM/EEM.EmbedTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EEM.EmbedTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running from within the binary");
+        }
+    }
+}
---
+++ EEM/EEM/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace EEM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bytes = new byte[0];
+            
+            var assembly = AppDomain.Load(bytes);
+            Type typeToExecute = assembly.GetType("Program");
+            Object instance = Activator.CreateInstance(typeToExecute);
+        }
+    }
+}
