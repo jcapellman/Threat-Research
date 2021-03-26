7defe1f3c7e81c2a4d728ee33789ee80e2f8bd2b
Jarred Capellman <jcapellman@hotmail.com>
2021-03-26 18:51:45 -0500

    Added: Initial work on MOTRA

---
+++ MOTRA/MotraAnalyzer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using DocumentFormat.OpenXml.Packaging;
+
+namespace MOTRA
+{
+    public class MotraAnalyzer
+    {
+        private static string ParsePart<T>(SpreadsheetDocument document, string name) where T : OpenXmlPart
+        {
+            var parts = document.WorkbookPart.WorksheetParts.Sum(a => a.GetPartsOfType<T>().Count());
+
+            // TODO: Output elements instead of just the count
+
+            return parts > 0
+                ? $"{name} - {parts} were found"
+                : $"{name} - None were found";
+        }
+
+        public List<string> Analyze(string fileName)
+        {
+            var analysis = new List<string>();
+
+            using (var stream = File.OpenRead(fileName))
+            {
+                try
+                {
+                    using (SpreadsheetDocument spreadsheet = SpreadsheetDocument.Open(stream, false))
+                    {
+                        analysis.Add(ParsePart<MacroSheetPart>(spreadsheet, "Macros"));
+                        analysis.Add(ParsePart<ConnectionsPart>(spreadsheet, "External Connections"));
+                        analysis.Add(ParsePart<VbaProjectPart>(spreadsheet, "VBA Project"));
+                        analysis.Add(ParsePart<EmbeddedObjectPart>(spreadsheet, "Embedded Objects"));
+                        analysis.Add(ParsePart<ImagePart>(spreadsheet, "Images"));
+                        analysis.Add(ParsePart<CustomDataPart>(spreadsheet, "Custom Data"));
+                        analysis.Add(ParsePart<VbaDataPart>(spreadsheet, "VBA Data"));
+                        analysis.Add(ParsePart<VmlDrawingPart>(spreadsheet, "VML Drawing"));
+                    }
+                }
+                catch (OpenXmlPackageException)
+                {
+                    analysis.Add("File is not a Modern Excel Document");
+                }
+            }
+
+            return analysis;
+        }
+    }
+}
---
+++ MOTRA/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace MOTRA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine($"{args[0]} does not exist - exiting");
+
+                return;
+            }
+
+            Console.WriteLine($"Analysis of {args[0]}:");
+
+            var analysis = new MotraAnalyzer().Analyze(args[0]);
+
+            var x = 1;
+
+            foreach (var line in analysis)
+            {
+                Console.WriteLine($"{x}.{line}");
+
+                x++;
+            }
+        }
+    }
+}
