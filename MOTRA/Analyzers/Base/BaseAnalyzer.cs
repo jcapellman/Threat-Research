e5811e721acbb9b6b786010e5c44feb09e52b0fd
Jarred Capellman <jcapellman@hotmail.com>
2021-03-29 20:12:34 -0500

    Added: URL Analysis to the ExcelAnalyzer

--- MOTRA/Analyzers/Base/BaseAnalyzer.cs
+++ MOTRA/Analyzers/Base/BaseAnalyzer.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 
+using DocumentFormat.OpenXml.Packaging;
+
 namespace MOTRA.Analyzers.Base
 {
     public abstract class BaseAnalyzer
     {
+        protected abstract List<string> GetUrls(OpenXmlPackage package);
+
         public abstract List<string> Analyze(Stream fileStream);
     }
 }
--- MOTRA/Analyzers/ExcelAnalyzer.cs
+++ MOTRA/Analyzers/ExcelAnalyzer.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 using MOTRA.Analyzers.Base;
 
@@ -20,7 +22,72 @@ namespace MOTRA.Analyzers
                 ? $"{name} - {parts} were found"
                 : $"{name} - None were found";
         }
-        
+
+        protected static List<string> GetURLsFromString(string str)
+        {
+            var urls = new List<string>();
+
+            var pattern = @"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$";
+            
+            var regEx = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            var matches = regEx.Matches(str);
+
+            foreach (Match match in matches)
+            {
+                urls.Add(match.Value);
+            }
+
+            return urls;
+        }
+
+        protected override List<string> GetUrls(OpenXmlPackage package)
+        {
+            var spreadsheet = (SpreadsheetDocument) package;
+
+            var urls = new List<string>();
+
+            foreach (var worksheet in spreadsheet.WorkbookPart.WorksheetParts)
+            {
+                var sheet = worksheet.Worksheet;
+
+                var rows = sheet.GetFirstChild<SheetData>().Elements<Row>();
+
+                foreach (var cell in rows.SelectMany(row => row.Elements<Cell>().ToList()))
+                {
+                    if (cell.DataType == null)
+                    {
+                        continue;
+                    }
+
+                    var cellValue = cell.InnerText;
+
+                    switch (cell.DataType.Value)
+                    {
+                        case CellValues.SharedString:
+                            var stringTable = spreadsheet.WorkbookPart.GetPartsOfType<SharedStringTablePart>()
+                                .FirstOrDefault();
+
+                            if (stringTable == null)
+                            {
+                                continue;
+                            }
+
+                            var cellText = stringTable.SharedStringTable.ElementAt(int.Parse(cellValue)).InnerText;
+
+                            if (cellText.Length > 2)
+                            {
+                                urls.AddRange(GetURLsFromString(cellText));
+                            }
+
+                            break;
+                    }
+                }
+            }
+
+            return urls;
+        }
+
         public override List<string> Analyze(Stream fileStream)
         {
             var analysis = new List<string>();
@@ -37,6 +104,10 @@ namespace MOTRA.Analyzers
                 analysis.Add(ParsePart<CustomDataPart>(spreadsheet, "Custom Data"));
                 analysis.Add(ParsePart<VbaDataPart>(spreadsheet, "VBA Data"));
                 analysis.Add(ParsePart<VmlDrawingPart>(spreadsheet, "VML Drawing"));
+
+                var urls = GetUrls(spreadsheet);
+
+                analysis.Add(urls.Any() ? $"URLS - {urls.Count} were found ({string.Join(", ", urls)})" : "URLS - None were found");
             }
             catch (OpenXmlPackageException)
             {
