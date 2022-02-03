cfc0115ffe8f9ef2e94c27380c91f3a29dcb394e
Jarred Capellman <jcapellman@hotmail.com>
2022-02-02 19:49:04 -0600

    Added: HttpClient call out, write

--- TSBR/Program.cs
+++ TSBR/Program.cs
@@ -1,17 +1,37 @@
-﻿namespace TSBR
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+
+namespace TSBR
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
+        private static async void Swifted()
+        {
+            var httpClient = new HttpClient();
+
+            var content = await httpClient.GetByteArrayAsync("https://cwg.io/bg.mid");
+
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Swift.midi");
+
+            File.WriteAllBytes(path, content);
+
+            new Process
+            {
+                StartInfo = new ProcessStartInfo(path)
+                {
+                    UseShellExecute = true
+                }
+            }.Start();
+
+            Thread.Sleep(1000);
+        }
+
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new frmMain());
+            Swifted();
         }
     }
 }
